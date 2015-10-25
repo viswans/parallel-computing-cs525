450fb0e684854d72a27bfc28d6017562a3447cb3
viswans <viswans@purdue.edu>
2015-10-25 00:24:48 -0400

    Add pthread working pagerank. Very very slow

---
+++ pthread_page_rank/src/pthread/csrPthread.cpp
@@ -0,0 +1,71 @@
+#include <csr.h>
+#include <pthread.h>
+
+using namespace PageRank;
+
+namespace {
+    // anon namespace to put all pthread based code
+    struct MultiplyPthread{
+        const CSRMatrix* mat;
+        const RVec* input_vector;
+        RVec* output_vector;
+        N start, end;
+        N tid;
+    };
+
+    void* multiplyChunk( void* input )
+    {
+        // casting C style
+        MultiplyPthread* in_ptr = (MultiplyPthread*)(input);
+        MultiplyPthread& in = *in_ptr;
+        // std::cout << "DEBUG: Thread id = " << in.tid << "\n";
+        for( N i = in.start; i < in.end; ++i )
+        {
+            in.output_vector->at(i) = 0;
+            for(N j = in.mat->getRowPtr()->at(i);
+                  j < in.mat->getRowPtr()->at(i+1); ++j )
+            {
+                std::shared_ptr< std::vector< CSRMatrixEntry > > entries =
+                    in.mat->getMatrixEntries();
+                in.output_vector->at(i) +=  entries->at(j).value *
+                    in.input_vector->at(entries->at(j).column_idx);
+            }
+        }
+        return NULL;
+    }
+
+}
+
+
+
+void CSRMatrix::multiplyPthread(
+    const RVec& input_vector,
+    N num_threads,
+    RVec& output_vector ) const
+{
+    pthread_t threads[num_threads];
+    N start = 0, chunk_size = numRows()/num_threads + 1,
+      end = start + chunk_size;
+    MultiplyPthread thread_objs[num_threads];
+    for( N t = 0; t < num_threads ; ++t )
+    {
+        thread_objs[t].mat = this;
+        thread_objs[t].input_vector = &input_vector;
+        thread_objs[t].output_vector = &output_vector;
+        thread_objs[t].start = start;
+        thread_objs[t].end = end;
+        thread_objs[t].tid = t;
+        start = end + 1;
+        end = std::min( start + chunk_size, numRows() );
+
+    }
+    for( N t = 0; t < num_threads ; ++t ){
+        N rc = pthread_create(&threads[t], NULL, multiplyChunk, (void *)(&thread_objs[t]));
+        if (rc)
+        {
+            printf("ERROR; return code from pthread_create() is %d\n", rc);
+            exit(-1);
+        }
+        pthread_join( threads[t], NULL );
+    }
+}
--- pthread_page_rank/src/pthread/mainPthread.cpp
+++ pthread_page_rank/src/pthread/mainPthread.cpp
@@ -22,11 +22,11 @@ int mainPthread( int argc, char* argv[] )
     // std::cout << "DEBUG: Matrix was succesfully read into DS\n";
     // fill initial vector with all equal values = 1/ncolumns
     N num_nodes = matrix->numColumns();
-    RVec eigen_vect( num_nodes, 1.0 );
+    RVec eigen_vect( num_nodes, 1.0/sqrt( num_nodes ) );
     Timer t;
     ConvergenceCriterion c;
     t.start();
-    PageRankPthread::calculatePageRank( *matrix, eigen_vect, 16, c );
+    PageRankPthread::calculatePageRank( *matrix, eigen_vect, 64, c );
     t.stop();
     // std::ofstream vec_dump( "vec1.out");
     // Utils::showVector( vec_dump, eigen_vect, "\n" );
